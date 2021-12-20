19b56a98118fae72a1edd436baa545e47f1ebf02
amu-little-princess <1794166870@qq.com>
2021-12-20 09:31:54 +0800

    首页背景

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,10 +5,23 @@ import Route from "../../router/router";
 
 class Header extends React.Component {
 
+    componentDidMount() {
+        window.addEventListener("scroll",this.handleScroll)
+    }
+
+    handleScroll = (e) =>{
+        const header = document.getElementById("header")
+        if(window.pageYOffset>=80){
+            header.classList.add("header-bg")
+        }else{
+            header.classList.remove("header-bg")
+        }
+    }
+
     render() {
         return (
             <Router>
-                <div className="header">
+                <div className="header" id="header">
                     <div className="nav">
                         <ul>
                             <li>
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,11 +1,20 @@
 import React from "react";
+import homeImg from '../../assets/home/home-background.jpeg'
+
+const homeStyle = {
+    backgroundImage:`url(${homeImg})`,
+    height:"100vh",
+    width:"100vw",
+    backgroundSize:"100%",
+    backgroundRepeat:"no-repeat"
+}
 
 class Home extends React.Component{
 
     render() {
         return (
             <div className="app">
-                首页
+                <div style={homeStyle} className="home-slogan"> jknj</div>
             </div>
         )
     }
